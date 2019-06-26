e552929cee2a12578a54971cf090a6adcf613e1c
Carmen79 <carmen.buevig@gmail.com>
2019-06-26 21:53:38 +0200

    travelling creation

---
+++ travelling/config/mongoDb.js
@@ -0,0 +1,19 @@
+//conexion a Mongo
+//Aqui no tenemos que decir módule export porque hemos 
+//creado una variable global que  podemos usar en todo el sistema
+
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017';
+
+const dbName = 'travelling';
+
+MongoClient.connect(
+    url,
+    {
+        useNewUrlParser: true
+    },
+    function (err, db) {
+        if (err) throw err;
+        global.dbo = db.db(dbName);
+    });
---
+++ travelling/routes/front.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/login', function (req, res, next) {
+    res.render('login');
+});
+router.get('/newuser', function (req, res, next) {
+    res.render('newuser');
+});
+
+
+router.get('/users', function (req, res, next) {
+    res.render('users');
+});
+
+router.get('/travels', function (req, res, next) {
+    res.render('travels');
+});
+
+router.get('/users/edit/:id', function (req, res, next){
+    res.render('edit');
+});
+
+module.exports = router;
